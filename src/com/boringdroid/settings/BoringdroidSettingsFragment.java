23dfdd66e6a147a0903e0d51c3c9aa0c2813dd3b
utzcoz <utzcoz@outlook.com>
2020-10-18 23:28:10 +0800

    Use current value for preference state
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- src/com/boringdroid/settings/BoringdroidSettingsFragment.java
+++ src/com/boringdroid/settings/BoringdroidSettingsFragment.java
@@ -13,6 +13,11 @@ import java.lang.reflect.Method;
 public class BoringdroidSettingsFragment extends PreferenceFragmentCompat {
     private static final String TAG = "BDSettingsFragment";
 
+    private static final String SYSTEM_PROPERTIES_CLASS_NAME = "android.os.SystemProperties";
+
+    private static final String PROPERTY_PC_MODE_KEY = "persist.sys.pcmode.enabled";
+    private static final String PROPERTY_BD_SYSTEMUI_KEY = "persist.sys.systemuiplugin.enabled";
+
     private SwitchPreferenceCompat mSwitchEnablePCMode;
     private SwitchPreferenceCompat mSwitchEnabledBoringdroidSystemUI;
 
@@ -23,10 +28,14 @@ public class BoringdroidSettingsFragment extends PreferenceFragmentCompat {
                 (SwitchPreferenceCompat) findPreference(
                         getString(R.string.key_switch_enable_pc_mode)
                 );
+        mSwitchEnablePCMode.setChecked(getBooleanSystemProperties(PROPERTY_PC_MODE_KEY));
         mSwitchEnabledBoringdroidSystemUI =
                 (SwitchPreferenceCompat) findPreference(
                         getString(R.string.key_switch_enable_bd_nav_bar)
                 );
+        mSwitchEnabledBoringdroidSystemUI.setChecked(
+                getBooleanSystemProperties(PROPERTY_BD_SYSTEMUI_KEY)
+        );
 
         mSwitchEnablePCMode.setOnPreferenceClickListener(preference -> {
             enablePCMode(((SwitchPreferenceCompat) preference).isChecked());
@@ -40,18 +49,18 @@ public class BoringdroidSettingsFragment extends PreferenceFragmentCompat {
 
     private void enablePCMode(boolean enable) {
         Log.d(TAG, "enable pc mode " + enable);
-        setBooleanSystemProperties("persist.sys.pcmode.enabled", enable);
+        setBooleanSystemProperties(PROPERTY_PC_MODE_KEY, enable);
     }
 
     private void enableBoringdroidSystemUI(boolean enable) {
         Log.d(TAG, "enable Boringdroid SystemUI " + enable);
-        setBooleanSystemProperties("persist.sys.systemuiplugin.enabled", enable);
+        setBooleanSystemProperties(PROPERTY_BD_SYSTEMUI_KEY, enable);
     }
 
     private void setBooleanSystemProperties(String key, boolean value) {
         try {
             @SuppressLint("PrivateApi")
-            Class clazz = Class.forName("android.os.SystemProperties");
+            Class clazz = Class.forName(SYSTEM_PROPERTIES_CLASS_NAME);
             Method setMethod = clazz.getMethod("set", String.class, String.class);
             setMethod.invoke(null, key, value ? "true" : "false");
         } catch (ClassNotFoundException
@@ -60,5 +69,20 @@ public class BoringdroidSettingsFragment extends PreferenceFragmentCompat {
                 | InvocationTargetException e) {
             Log.d(TAG, "Failed to set value " + value + " for " + key);
         }
+    }
+
+    private boolean getBooleanSystemProperties(String key) {
+        try {
+            @SuppressLint("PrivateApi")
+            Class clazz = Class.forName(SYSTEM_PROPERTIES_CLASS_NAME);
+            Method setMethod = clazz.getMethod("getBoolean", String.class, boolean.class);
+            return (boolean) setMethod.invoke(null, key, true);
+        } catch (ClassNotFoundException
+                | NoSuchMethodException
+                | IllegalAccessException
+                | InvocationTargetException e) {
+            Log.d(TAG, "Failed to get value for " + key);
+        }
+        return true;
     }
 }
