a3bff7d78f943c8a12bab00a31831c1e4d5d6769
utzcoz <utzcoz@outlook.com>
2020-12-26 19:15:23 +0800

    Add back button for settings page
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- src/com/boringdroid/settings/BoringdroidSettings.java
+++ src/com/boringdroid/settings/BoringdroidSettings.java
@@ -3,6 +3,7 @@ package com.boringdroid.settings;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BoringdroidSettings extends AppCompatActivity {
@@ -14,5 +15,18 @@ public class BoringdroidSettings extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.settings_container, new BoringdroidSettingsFragment())
                 .commit();
+
+        // Enable back button for preference activity
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
