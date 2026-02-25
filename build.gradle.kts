69e6f155b3cf0cef1e10c1b19257757b9a1a2b39
utzcoz <utzcoz@outlook.com>
2026-02-26 00:51:11 +0800

    Migrate build JDK to 21
    Signed-off-by: utzcoz <utzcoz@outlook.com>

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "9.0.0" apply false
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+    }
+}
